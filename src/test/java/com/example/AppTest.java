c5087dab8007b298fa3d42a64665393c7ba23615
S Purushotham Shreyas Reddy <purushotham.shreyas2023@vitstudent.ac.in>
2026-03-05 23:17:15 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -7,4 +7,22 @@ public void testAdd() {
 App app = new App();
 assertEquals(5, app.add(2, 3));
 }
+
+@Test
+  public void testSub() {
+  App app = new App();
+  assertEquals(5, app.sub(5,6));
+}
+
+  @Test
+  public void testMul() {
+    App app = new App();
+    assertEquals(5, app.mul(9,8));
+  }
+
+  @Test
+  public void testDiv() {
+    App app = new App();
+    assertEquals(5, app.div(10,5));
+  }
 }
